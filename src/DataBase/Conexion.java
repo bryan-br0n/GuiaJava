84d82b8ba8f68a2553c6040e268410617fc6f097
bryan-br0n <126531322+bryan-br0n@users.noreply.github.com>
2025-02-27 18:06:06 -0600

    Guia #5

---
+++ src/DataBase/Conexion.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jccam
+ */
+public class Conexion {
+    private final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    private final String URL="jdbc:mysql://localhost:3306/";  
+    private final String DB = "puntoventa";
+    private final String USER = "root";
+    private final String PASSWORD = "Quintanill4"; 
+
+public Connection connection ;
+public Conexion() {
+this.connection = null;
+}
+public  Connection conectar (){
+        try {
+            Class .forName(DB_DRIVER);
+            this.connection = DriverManager.getConnection(
+                    URL + DB, USER, PASSWORD);
+                   
+        } catch ( ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            System.exit(0);
+        }
+        return  this.connection;
+    }
+
+public void desconectar(){
+    try {
+        this.connection.close();
+    } catch (SQLException e){
+        JOptionPane.showMessageDialog(null, e.getMessage());
+    }
+}
+}
